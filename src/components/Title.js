455393ada1e1625f13bf30df9b90dbe5c37d76c3
Luke Moule <lukemoule20@gmail.com>
2023-07-01 22:51:40 +0100

    add components

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import api from '../images/api.jpg'
+import logo from '../images/logo.jpg'
+
+function Title() {
+    return (
+        <>
+        <div className="logo__api"><a href="https://gnews.io/" target="_blank"><img src={api} alt="GNews API" /></a></div>
+        <div className="logo__title"><a href="https://www.nytimes.com/ " target="_blank"><img src={logo} alt="The React News App" /></a></div>
+        </>
+        )
+}
+
+export default Title
+
