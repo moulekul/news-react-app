1e91c234bf82854fd91e1f8cfa998322d9877082
Luke Moule <lukemoule20@gmail.com>
2023-07-05 20:15:20 +0100

    update grid display

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,31 +2,34 @@ function NewsItem({item}) {
     const websiteUrl = item.url
     const website = websiteUrl.split('https://').pop().split('/')[0]
 
-    const date = item.publishedAt
-    const formatDate = date.replace('T', ' ')
-    const formatTime = formatDate.replace('Z', '')
 
     return (
-        <a href={item.url} className="article">
-            <div className="article-image">
-                <img src={item.image} alt={item.title} />
+        <a href={item.url} target="_blank" className="article">
+            
+            <div className="article__title-container">
+            
+                <div className="article-title">
+                    <h2>{item.title}</h2>
+                </div>
+                <div className="article-description">
+                    <p>{item.description}</p>
+                </div>
+
             </div>
-            <div className="article-content">
+
+                <div className="article-image">
+                    <img src={item.image} alt={item.title} />
+                </div>
+            
+            <div className="article-div"></div>
+
                 <div className="article-source">
                     <img src={`https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,
                     URL&url=http://${website}&size=16`} alt={item.source.id} />
                     <span>{item.source.name}</span>
                 </div>
-                <div className="article-title">
-                    <h2>{item.title}</h2>
-                </div>
-                <p className="article-description">
-                   {item.description}
-                </p>
-                <div className="article-details">
-                    <small><b>Published At: </b>{formatTime}</small>
-                </div>
-            </div>
+
+            <div className="heading-line1"></div>
         </a>
     )
 }
